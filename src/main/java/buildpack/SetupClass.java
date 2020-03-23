7d0d7e034b4114b2cd8d17b69090ccc3100f0814
Sree <Sree@DESKTOP-GL4IC1U>
2020-03-23 22:07:55 +0530

    final project jpet

--- src/main/java/buildpack/SetupClass.java
+++ src/main/java/buildpack/SetupClass.java
@@ -4,13 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 
 public class SetupClass {
